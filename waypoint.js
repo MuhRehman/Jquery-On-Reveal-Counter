7f43c3a6378a4064ba71f0763366656d87802cd0
MuhRehman <paki_jan@yahoo.com>
2020-11-23 01:25:33 +0500

    rehman

---
+++ waypoint.js
@@ -0,0 +1,11 @@
+
+
+
+var $test = $('.ani');
+$test.waypoint(function () {
+              $test.addClass('.slide');
+              console.log("r");
+     
+});
+
+$(".num").counterUp({delay:10,time:2000});
